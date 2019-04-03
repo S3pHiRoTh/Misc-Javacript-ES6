5bb10b064a9109b84e1ceca12bd1995abb6f193b
A.Taylor <ashtrix2k@googlemail.com>
2019-04-03 22:13:18 +0100

    Updated lottoGen.js & Index.htm
    Cleaned up the code and added another function, SpecifySlips() which manages the amount of slips a user wishes to generate.
    
    Modified the form IDs to match the variable names in the script, to avoid confusion.

--- Lottery Generator/scripts/lottoGen.js
+++ Lottery Generator/scripts/lottoGen.js
@@ -6,8 +6,8 @@ Author: A.Taylor
 Language: JavaScript
 */
 
-// Amount of slips the user wants to generate
-let slipAmount = document.querySelectorAll("slipsToGenerate");
+// Grab the text field element
+let userInput = document.getElementById('userInput').value;
 
 // variable to reference the HTML element to apply the lotto results to
 let logSlips = document.getElementById('slipResults');
@@ -20,44 +20,61 @@ let generatedNumbers = [];
 
 // Create lottoResults as a global variable so our other functions can access it
 
+function SpecifySlips(){
+	let stopGenerating = 0;
+	console.log("button clicked");
+	
+	// loop catches the amount of slips the user has requested and compares it to the stop generating variable, for each iteration where the variable does not match, 
+	// it will continue to increment stopGenerating until they both match, then exiting the loop
+	for(i = document.getElementById('userInput').value; i != stopGenerating; stopGenerating++){
+			GenerateSlips();
+	}
+	
+}
+
 function GenerateSlips(){
-		console.log("button clicked");
-		
-		 while(generatedNumbers.length < 6){
-			// Randomly generate our lottery numbers, using math.random. This is also declared inside the loop as math.random will assign a different number each iteration.
-			let lottoNumReg = Math.floor(Math.random() *59) +1;
-			// Checking for duplicate numbers, if duplicate numbers are found, let's pop them out of the generatedNumbers until there aren't any. Putting this at the top so that it is the first thing that get's parsed in the loop
-			// Basically we are saying here is if the lotto number that has been generated matches any one in the array, delete the dupe from the array. Another number has not been pushed to the array while in the conditional block, as I want the loop to keep iterating until there are no dupes.
-			if(lottoNumReg === generatedNumbers[0]){
-				// outputting the matching dupe numbers to the console
-				console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[0]}`);
-				generatedNumbers.shift();
-			}
-			else if(lottoNumReg === generatedNumbers[1]){
-				console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[1]}`);
-				generatedNumbers.splice(1);
-			}
-			else if(lottoNumReg === generatedNumbers[2]){
-				console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[2]}`);
-				generatedNumbers.splice(2);
-			}
-			else if(lottoNumReg === generatedNumbers[3]){
-				console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[3]}`);
-				generatedNumbers.splice(3);
-			}
-			else if(lottoNumReg === generatedNumbers[4]){
-				console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[4]}`);
-				generatedNumbers.splice(4);
-			}
-			else if(lottoNumReg === generatedNumbers[5]){
-				console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[5]}`);
-				generatedNumbers.pop();
-			}
+	while(generatedNumbers.length < 6){
+				
+		// Randomly generate our lottery numbers, using math.random. This is also declared inside the loop as math.random will assign a different number each iteration.
+		let lottoNumReg = Math.floor(Math.random() *59) +1;
+				
+		// Checking for duplicate numbers, if duplicate numbers are found, let's pop them out of the generatedNumbers until there aren't any. Putting this at the top so that it is the first thing that get's parsed in the loop
+		// Basically we are saying here is if the lotto number that has been generated matches any one in the array, delete the dupe from the array. Another number has not been pushed to the array while in the conditional block, 
+		// as I want the loop to keep iterating until there are no dupes.
 			
-			// once the dupe check has been done (the conditional if statements above in the loop should take care of this dynamically now) and push the number to the array and store it
-			// assign the random generated number to the generatedNumbers
-			generatedNumbers.push(lottoNumReg);
+		if(lottoNumReg === generatedNumbers[0]){
+			// outputting the matching dupe numbers to the console
+			console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[0]}`);
+			generatedNumbers.shift();
 		}
+		else if(lottoNumReg === generatedNumbers[1]){
+			console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[1]}`);
+			generatedNumbers.splice(1);
+		}
+		else if(lottoNumReg === generatedNumbers[2]){
+			console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[2]}`);
+			generatedNumbers.splice(2);
+		}
+		else if(lottoNumReg === generatedNumbers[3]){
+			console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[3]}`);
+			generatedNumbers.splice(3);
+		}
+		else if(lottoNumReg === generatedNumbers[4]){
+			console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[4]}`);
+			generatedNumbers.splice(4);
+		}
+		else if(lottoNumReg === generatedNumbers[5]){
+			console.log(`Duplicate found. Lotto number that was generated: ${lottoNumReg} matches ${generatedNumbers[5]}`);
+			generatedNumbers.pop();
+		}
+			
+		// once the dupe check has been done (the conditional if statements above in the loop should take care of this dynamically now) and push the number to the array and store it
+		// assign the random generated number to the generatedNumbers
+			
+		generatedNumbers.push(lottoNumReg);
+			
+	}
+
 		
 		// outputting the result to the console so we can cross reference this with the actual output on the website
 		console.log(generatedNumbers);
@@ -69,8 +86,8 @@ function GenerateSlips(){
 		// Since the output has already been displayed, let's reset the variable 
 		generatedNumbers = [];
 
+	
 }
-
 // function to create some new baby elements and append them to the website dynamically.
 
 function createSlipElement(lottoResults){
